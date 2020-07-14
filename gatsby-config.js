378b532bcae4b9246203facc9df1096b07334d66
webdril <jimbahamas03@yahoo.com>
2020-07-14 09:28:19 +0000

    working new graphql playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log('Using environment config: ${activeEnv}')
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   /* Your site config here */
-<<<<<<< HEAD
-=======
   siteMetadata:{
     title: 'Full-Stack Bootcamp!',
     author: 'Emeka Eluwa'
   },
->>>>>>> c1a2efbe59c22d124b013fe1dc6f68f04c8886d9
   plugins: [
     'gatsby-plugin-sass'
   ]
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-
-const Footer = () =>{
-    return(
-        <footer>
-            <p>Created by Emeka Eluwa, &copy;2020</p>
-=======
 import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () =>{
@@ -22,7 +15,6 @@ const Footer = () =>{
     return(
         <footer>
             <p>Created by {data.site.siteMetadata.author}, &copy;2020</p>
->>>>>>> c1a2efbe59c22d124b013fe1dc6f68f04c8886d9
         </footer>
     )
 }
